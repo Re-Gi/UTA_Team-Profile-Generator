391ce9d51a7e3b6a6fb793ad7b8f21bb0ed21e68
Rebecca Girndt <r.l.girndt@gmail.com>
2023-01-30 17:38:25 -0600

    finished all tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,32 +1,33 @@
 const Employee = require('../lib/Employee');
 
+const employee = new Employee("Jane", 5117, "jane@email.com");
+
 describe("Employee class", () => {
     describe("Initialization", () => {
         it("should take name, id, and email values and create a new object", () => {
-            const employee = new Employee("Sharon", 1, "sharon@email.com");
             
-            expect(employee.name).toEqual("Sharon");
-            expect(employee.id).toEqual(1);
-            expect(employee.email).toEqual("sharon@email.com");
+            expect(employee.name).toEqual("Jane");
+            expect(employee.id).toEqual(5117);
+            expect(employee.email).toEqual("jane@email.com");
         });
     });
     
     describe("get methods", () => {
         it("should return the corresponding values of the employee their called on.", () => {
-            const employee = new Employee("Mark", 25, "mark@email.com");
             
-            expect(employee.getName()).toEqual("Mark");
-            expect(employee.getId()).toEqual(25);
-            expect(employee.getEmail()).toEqual("mark@email.com");
-            
+            expect(employee.getName()).toEqual("Jane");
+            expect(employee.getId()).toEqual(5117);
+            expect(employee.getEmail()).toEqual("jane@email.com");
         });
     });
 
     describe("getRole method", () => {
         it("should return 'Employee' on variables of the Employee class.", () => {
-            const employee = new Employee();
+
+            const notEmployee = "Employee"
             
             expect(employee.getRole()).toEqual("Employee");
+            expect(() => notEmployee.getRole()).toThrow(Error);
         });
     });
 });
